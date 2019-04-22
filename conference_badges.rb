c6191ef7225957d08c6c5a9ed17ca126f355343d
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-22 00:20:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.each_with_index |speaker, index|
+  array.each_with_index do |speaker, index|
   return "Hello, #{speaker}! You'll be assigned to room #{index}!"
 end
