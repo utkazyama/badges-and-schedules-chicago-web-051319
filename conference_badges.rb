60d9b638206d0aecfc2f9af68d5905f00c352fce
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-22 00:45:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  list=[]
+  attendees.each do |speaker|
+   list.push("Hello, my name is #{speaker}.")
+  end
+  return list
+end
+
+def assign_rooms(array)
+  list=[]
+  array.each_with_index do |speaker, index|
+  list.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
+ end
+ return list
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+   puts badge
+  end
+   
+   assign_rooms(attendees).each do |rooms|
+     puts rooms
+   end
+end
+
